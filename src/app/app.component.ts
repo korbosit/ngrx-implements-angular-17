ef2e9051858019a81981e480b710d829cdb6271d
Vlaimir Korba <krissky1986@gmail.com>
2024-03-13 14:57:55 +0200

    implement ngrx in Angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { increment, decrement, reset } from './counter.actions';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ngrx-implements-angular-17';
+
+  private store = inject(Store);
+
+  count$?: Observable<number>;
+
+  constructor() {
+    this.count$ = this.store.select('counter');
+  }
+
+  increment() {
+    this.store.dispatch(increment());
+  }
+  decrement() {
+    this.store.dispatch(decrement());
+  }
+  reset() {
+    this.store.dispatch(reset());
+  }
 }
